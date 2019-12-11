9d6e556c461e6259392fc454a2a41459951fe3a1
Muhammad Supriyadi <musupadi159@gmail.com>
2019-12-11 18:57:59 +0700

    DONE

--- app/src/main/java/com/destinyapp/skripsiapps/API/RetroServer.java
+++ app/src/main/java/com/destinyapp/skripsiapps/API/RetroServer.java
@@ -1,16 +1,25 @@
 package com.destinyapp.skripsiapps.API;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 
 public class RetroServer {
-    private static final String base_url = "http://musupadilatihan.000webhostapp.com/BiodataPahlawan/API/";
+    private static final String base_url = "http://destinyconsultant.tech/API/BiodataPahlawan/API/";
     private static Retrofit retrofit;
 
     public static Retrofit getClient(){
         if(retrofit==null){
+            OkHttpClient.Builder clientBuilder = new OkHttpClient.Builder();
+            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            clientBuilder.addInterceptor(loggingInterceptor);
             retrofit = new Retrofit.Builder()
                     .baseUrl(base_url)
+                    .client(clientBuilder.build())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
